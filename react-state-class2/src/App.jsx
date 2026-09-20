00bc254100a2d2eb3505e85c4ab386f0bc328f56
deepaksangwan <sangwandeepak008@gmail.com>
2026-09-20 23:19:19 +0530

    useEffect in react

--- react-state-class2/src/App.jsx
+++ react-state-class2/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Form from './Form'
 import CommentsForm from './CommentsForm'
 import Comment from './Comment'
+import Counter from './Counter'
 
 function App() {
   
 
   return (
     <>
-    <Comment/>
+    <Counter/>
 
     </>
   )
---
+++ react-state-class2/src/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function Counter(){
+
+    let [countx, setCountx] = useState(0);
+    let [county, setCounty] = useState(0);
+
+    let incCountx = () =>{
+        setCountx((currCount) => currCount + 1);
+    }
+   
+    let incCounty = () =>{
+        setCounty((currCount) => currCount + 1);
+    }
+
+    useEffect(function printSomething(){
+        console.log("this is a side-effect");
+    }, [countx])
+
+    return(
+        <div>
+            <h3>Count is = {countx}</h3>
+            <button onClick={incCountx}>+1 </button>
+  
+            <h3>Count is = {county}</h3>
+            <button onClick={incCounty}>+1 </button>
+        </div>
+    );
+}
