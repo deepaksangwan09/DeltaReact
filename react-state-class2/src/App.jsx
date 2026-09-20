e2cffb28e381363c6c416bb32c35266f195ff370
deepaksangwan <sangwandeepak008@gmail.com>
2026-09-20 20:46:31 +0530

    react forms

--- react-state-class2/src/App.jsx
+++ react-state-class2/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Form from './Form'
 import CommentsForm from './CommentsForm'
+import Comment from './Comment'
 
 function App() {
   
 
   return (
     <>
-    <CommentsForm/>
+    <Comment/>
+
     </>
   )
 }
--- react-state-class2/src/CommentsForm.jsx
+++ react-state-class2/src/CommentsForm.jsx
@@ -1,48 +1,72 @@
 import { useState } from "react"
-export default function CommentsForm(){
+import { useFormik} from "formik"
 
-    let [formData, setFormData] = useState({
-        username: "",
-        remarks: "",
-        rating : 5,
+ const validate = values => {
+   const errors = {};
+   if (!values.userName) {
+     errors.userName = 'Username cannot be empty';
+   } 
+   return errors;
+};
+
+export default function CommentsForm({addNewComment}){
+
+    // let [formData, setFormData] = useState({
+    //     username: "",
+    //     remarks: "",
+    //     rating : 5,
+    // });
+
+    const formik = useFormik({
+        initialValues : {
+        userName: '',
+        remarks: '',
+        rating: 5,
+       },
+       validate,
+       onSubmit : values => {
+        alert(JSON.stringify(values, null, 2));
+       },
     });
 
-    let handleInputChange = (event) =>{
-        setFormData((currData) =>{
-            return { ...currData , [event.target.name] : event.target.value}
-        });   
-    }
+    // let handleInputChange = (event) =>{
+    //     setFormData((currData) =>{
+    //         return { ...currData , [event.target.name] : event.target.value}
+    //     });   
+    // }
 
-    let handleSubmit = (event) =>{
-        console.log(formData);
-        event.preventDefault();
-        setFormData({
-            username: "",
-            remarks: "",
-            rating : 5,
-        });
-    }
+    // let handleSubmit = (event) =>{
+    //     console.log(formData);
+    //     addNewComment(formData);
+    //     event.preventDefault();
+    //     setFormData({
+    //         username: "",
+    //         remarks: "",
+    //         rating : 5,
+    //     });
+    // }
     return(
         <div>
             <h4>Give a comment!</h4>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="username">Username</label>
                 <input 
                     placeholder = "username"
                     type="text" 
-                    value={formData.username} 
-                    onChange={handleInputChange} 
+                    value={formik.values.username} 
+                    onChange={formik.handleChange} 
                     id="username" 
                     name="username"
                 />
+                {formik.errors.userName ? <p style={{color:"red"}}>{formik.errors.userName}</p> : null}
 
                 <br /><br /><br />
-
+ 
                 <label htmlFor="remarks">Remarks</label>
                 <textarea 
-                    value={formData.remarks} 
+                    value={formik.values.remarks} 
                     placeholder="add few remarks"  
-                    onChange={handleInputChange} 
+                    onChange={formik.handleChange} 
                     id="remarks" 
                     name="remarks" 
                 ></textarea>
@@ -53,14 +77,14 @@ export default function CommentsForm(){
                     placeholder = "rating" 
                     type="number" 
                     min={1} max={5}  
-                    value={formData.rating}  
-                    onChange={handleInputChange} 
+                    value={formik.values.rating}  
+                    onChange={formik.handleChange} 
                     id="rating" 
                     name="rating"
                 />
                 <br /><br /><br />
 
-                <button>Add Comment</button>
+                <button type="submit">Add Comment</button>
             </form>
         </div>
     )
