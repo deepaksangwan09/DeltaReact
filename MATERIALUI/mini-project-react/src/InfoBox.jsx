34affa62cef3c841d2dde34926b83389fd4f34f6
deepaksangwan <sangwandeepak008@gmail.com>
2026-09-21 16:29:15 +0530

    Add weather icons

--- MATERIALUI/mini-project-react/src/InfoBox.jsx
+++ MATERIALUI/mini-project-react/src/InfoBox.jsx
@@ -2,12 +2,18 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import SunnyIcon from '@mui/icons-material/Sunny';
+import SevereColdIcon from '@mui/icons-material/SevereCold';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import "./InfoBox.css";
 
 export default function InfoBox({info}){
 
     let INIT_URL = "https://images.unsplash.com/photo-1641970304222-b2b332808a4b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww"
 
+    const HOT_URL = "https://images.unsplash.com/photo-1448317971280-6c74e016e49c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8SG90JTIwTmV3eW9ya3xlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1608475035939-be57a563e02c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c25vdyUyME5ld3lvcmt8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL = "https://images.unsplash.com/photo-1569684533256-5ccb7e30377f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8UkFJTiUyMENJVFl8ZW58MHx8MHx8fDA%3D";
 
     return(
         <div className="InfoBox">
@@ -15,11 +21,16 @@ export default function InfoBox({info}){
             <Card sx={{ maxWidth: 345 }}>
                 <CardMedia
                 sx={{ height: 140 }}
-                image={INIT_URL}
+                image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL }
                 title="green iguana"
             />
             <CardContent>
-            <Typography gutterBottom variant="h5" component="div">{info.city}</Typography>
+            <Typography gutterBottom variant="h5" component="div">
+                {info.city}{
+                info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <SunnyIcon/> : <SevereColdIcon/>
+                }
+
+            </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
                 <p>Temperature = {info.temp}&deg;C</p>
                 <p>Humidity = {info.humidity}</p>
